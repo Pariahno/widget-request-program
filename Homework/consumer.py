9fc9b7d7e09f0b29e132bbd043856e4f2bff66be
David Rasmussen <david.ras97@gmail.com>
2023-10-30 19:23:15 +0000

    Began work on implementing queue retrieval

--- Homework/consumer.py
+++ Homework/consumer.py
@@ -10,36 +10,46 @@ logging.basicConfig(filename="consumer.log", level=logging.INFO)
 # Read and process command line arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-rb", "--request-bucket", help="source bucket of widget requests")
+ap.add_argument("-rq", "--request-queue", help="source queue of widget requests")
 ap.add_argument("-wb", "--widget-bucket", help="destination bucket to send widgets")
 ap.add_argument("-wt", "--widget-table", help="destination database table to send widgets")
 args = ap.parse_args()
 
 def main(args):
     rb_name = args.request_bucket
+    rq_name = args.request_queue
     wb_name = args.widget_bucket
     wt_name = args.widget_table
 
     s3_resource = boto3.resource('s3')
+    sqs_resource = boto3.resource('sqs')
     dynamo_db_resource = boto3.resource('dynamodb')
     
     # Checking for existing resources from arguments
     bucket_list = get_bucket_list(s3_resource)
+    queue_list = get_queue_list(sqs_resource)
     table_list = get_table_list(dynamo_db_resource)
-    if rb_name == None or (wb_name == None and wt_name == None):
+    if (rb_name == None and rq_name == None) or (wb_name == None and wt_name == None):
         logging.error('Insufficient arguments specified')
         print("Insufficient arguments specified")
         return
-    elif not rb_name in bucket_list:
-        logging.error('Request bucket not found')
-        print("Request bucket not found")
+    elif (not rb_name == None and not rb_name in bucket_list) or (not rq_name == None and not rq_name in queue_list):
+        logging.error('Request resource not found')
+        print("Request resource not found")
         return
-    elif wb_name not in bucket_list or wt_name not in table_list:
-        logging.error('Destination bucket or table resources not found')
-        print("Resources not found")
+    elif (not wb_name == None and not wb_name in bucket_list) or (not wt_name == None and not wt_name in table_list):
+        logging.error('Destination resources not found')
+        print("Destination resources not found")
+        return
     else:
-        rb = s3_resource.Bucket(rb_name)
+        rb = None
+        rq = None
         wb = None
         wt = None
+        if rb_name in bucket_list:
+            rb = s3_resource.Bucket(rb_name)
+        if rq_name in queue_list:
+            rq = sqs_resource.Queue(rq_name)
         if wb_name in bucket_list:
             wb = s3_resource.Bucket(wb_name)
         if wt_name in table_list:
@@ -112,6 +122,13 @@ def get_bucket_list(s3_resource):
         bucket_list.append(bucket.name)
 
     return bucket_list
+    
+def get_queue_list(sqs_resource):
+    queue_list = []
+    for queue in sqs_resource.queues.all():
+        queue_list.append(queue.name)
+        
+    return queue_list
     
     
 # Returns a list of strings of existing table names
